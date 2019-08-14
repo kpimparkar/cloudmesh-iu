b0ddf12f5092fe1a32b7b483834ee9bfeecefda9
Gregor von Laszewski <laszewski@gmail.com>
2019-08-14 11:36:29 -0400

    add statsu command

--- cloudmesh/iu/api/manager.py
+++ cloudmesh/iu/api/manager.py
@@ -1,7 +1,88 @@
+import os
+from subprocess import check_output
+from cloudmesh.common3.Shell import Shell
+from cloudmesh.common.Printer import Printer
+from textwrap import indent
 class Manager(object):
 
+    """
+    PARTITION  AVAIL  TIMELIMIT  NODES   GRES  STATE NODELIST
+    romeo         up   infinite      4  gpu:8    mix r-[001-004]
+    volta         up   infinite      2  gpu:8    mix r-[005-006]
+    """
     def __init__(self):
         print("init {name}".format(name=self.__class__.__name__))
 
     def list(self, parameter):
         print("list", parameter)
+
+    def login(self,
+              user=None,
+              host="romeo",
+              node=1,
+              gpus=1):
+
+        command = f"ssh -t {user}@juliet.futuresystems.org " \
+                  f" srun -p {host}" \
+                  f" -w {node} --gres gpu:{gpus} --pty bash"
+
+        os.system(command)
+
+    def smart_login(self,
+              user=None,
+              host="romeo",
+              node=1,
+              gpus=1):
+        status = self.queue(host=host, user=user)
+        print (Printer.attribute(status, header=["Node", "Used GPUs"]))
+
+    def status(self, user=None):
+
+        for host in ["romeo", "volta"]:
+
+            status = self.queue(host=host, user=user)
+            print(Printer.attribute(status, header=[host, "Used GPUs"]))
+
+        for host in ["romeo", "volta"]:
+            users = self.users(host=host, user=user)
+            print()
+            print (f"Users on {host}")
+            print()
+            print (indent("\n".join(users), "    "))
+        print()
+
+
+    def users(self, host=None, user=None):
+        command = f"ssh -o LogLevel=QUIET -t {user}@juliet.futuresystems.org " \
+                  f" squeue -p {host} -o \"%u\""
+        r = check_output(command, shell=True).decode('ascii').split()[1:]
+        r = sorted(set(r))
+        return r
+
+    def queue(self, host=None, user=None):
+        command = f"ssh -o LogLevel=QUIET -t {user}@juliet.futuresystems.org " \
+                  f" squeue -p {host}"
+        # print(command)
+
+        lines = check_output(command, shell=True).decode('ascii') \
+                    .splitlines()[1:]
+
+        used = {}
+
+        for line in lines:
+            attributes = line.split()
+            gpus = attributes[7]
+            try:
+                gpus = int(gpus.split(":")[1])
+            except:
+                gpus = 0
+            host = attributes[8]
+            if "Unavailable" in host:
+                continue
+            if host not in used:
+                used[host] = 0
+            used[host] += gpus
+            # print (attributes)
+
+
+        return used
--- cloudmesh/iu/command/iu.py
+++ cloudmesh/iu/command/iu.py
@@ -1,11 +1,18 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
-from cloudmesh.shell.command import PluginCommand
+from cloudmesh.shell.command import PluginCommand, map_parameters
 from cloudmesh.iu.api.manager import Manager
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 from pprint import pprint
+from cloudmesh.common.parameter import Parameter
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.variables import Variables
+from cloudmesh.common.util import banner
+from cloudmesh.common.Printer import Printer
 
+from cloudmesh.configuration.Config import Config
+import random
 
 class IuCommand(PluginCommand):
 
@@ -16,31 +23,71 @@ class IuCommand(PluginCommand):
         ::
 
           Usage:
-                iu --file=FILE
-                iu list
+                iu [--user=USERNAME] [--host=HOST] [--node=NUMBER] [--gpu=GPUS]
+                iu status
 
           This command does some useful things.
 
           Arguments:
               FILE   a file name
+              HOST   the host is either rome or volta [default: romeo]
 
           Options:
               -f      specify the file
 
         """
-        arguments.FILE = arguments['--file'] or None
 
-        print(arguments)
+        map_parameters(arguments,
+                       "user",
+                       "host",
+                       "node",
+                       "gpu")
 
-        m = Manager()
+        variables = Variables()
+        arguments["user"] = Parameter.find("user", arguments, variables)
 
-        if arguments.FILE:
-            print("option a")
-            m.list(path_expand(arguments.FILE))
+        if arguments.user is None:
+            config = Config()
+            arguments.user = config["cloudmesh.iu.user"]
 
-        elif arguments.list:
-            print("option b")
-            m.list("just calling list without parameter")
+        iu = Manager()
 
-        Console.error("This is just a sample")
+
+        if arguments.status:
+
+            iu.status(user=arguments.user)
+
+            return ""
+
+
+
+
+
+        arguments["host"] = Parameter.find("host", arguments, variables,
+                                           {"host": "romeo"})
+        arguments["node"] = Parameter.find("node", arguments, variables)
+        arguments["gpu"] = int(Parameter.find("gpu", arguments, variables,
+                                          {"gpu": "1"}))
+
+
+        if arguments.node is None:
+
+            if arguments.host == "volta":
+                random.randint(1, 2) + 4
+
+            else:
+                number = random.randint(1, 4)
+            arguments.node = f"r-00{number}"
+
+
+        VERBOSE(arguments)
+
+        banner("Login")
+
+
+        iu.smart_login(user=arguments.user,
+                 host=arguments.host,
+                 node=arguments.node,
+                 gpus=arguments.gpu)
+
         return ""
